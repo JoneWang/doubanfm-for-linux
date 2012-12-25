d404a0765f7e67e1b86f223a5e954455db0ece94
JoneWang <i.jonewang@gmail.com>
2012-12-25 13:36:32 +0800

    Added channel list.User can select channel.

--- src/doubanfm.py
+++ src/doubanfm.py
@@ -1,25 +1,65 @@
+# coding=utf-8
+
 import urllib
 import json
 import pynotify
 import sys
 import time
 import mdecode
+import re
 
-fmurl= 'http://douban.fm/j/mine/playlist?'
+channelurl = 'http://douban.fm/j/explore/hot_channels'
+
+channelurl_list_params = {
+            'start': '0',
+            'limit': '10',
+        }
+
+fmurl= 'http://douban.fm/j/mine/playlist'
 
 music_list_params = {
             'type': 'n',
             'channel': '32',
             'from': 'mainsite',
+            'r': '73072e1c06',
         }
 
-def get_music_list():
-    params_data = urllib.urlencode(music_list_params)
-    print params_data
-    data = urllib.urlopen(''.join([fmurl, params_data]))
-    music_list = data.read()
-    print music_list
-    return json.loads(music_list)['song']
+def get_channel_list(page_num):
+    limit = 20
+    start = page_num*limit
+    channelurl_list_params['start'] = start
+    channelurl_list_params['limit'] = limit
+    channel_list_data = _request_url(channelurl, channelurl_list_params)
+    channel_list = json.loads(channel_list_data)['data']['channels']
+    if channel_list == []:
+        return False
+    print ''.ljust(55, '-')
+    print '|', 'Channel'.ljust(25, ' ') ,' ID '.ljust(12, ' '), 'Music Count'.ljust(12, ' '), '|'
+    print ''.ljust(55, '-')
+    for id in range(len(channel_list)):
+        channel = channel_list[id]
+        name = channel['name']
+        song_num = str(channel['song_num'])
+        name_zh = len_zh(name)
+        print '|', ('%s MHz' % name).ljust(24-name_zh, ' '), (' [%s] ' % channel['id']).ljust(15, ' '), song_num.ljust(10, ' '), '|'
+    print ''.ljust(55, '-')
+    print '|', ('Current Page: %s' % (page_num+1)).ljust(51, ' '), '|'
+    print ''.ljust(55, '-')
+    return True
+
+def get_music_list(channel_id):
+    music_list_params['channel'] = str(channel_id)
+    music_list = _request_url(fmurl, music_list_params)
+    data = json.loads(music_list)
+    if data['r'] == 0:
+        return json.loads(music_list)['song']
+    else:
+        return []
+
+def _request_url(url, params):
+    params_data = urllib.urlencode(params)
+    data = urllib.urlopen('?'.join([url, params_data]))
+    return data.read()
 
 
 if not pynotify.init ("icon-summary"):
@@ -49,20 +89,62 @@ def initCaps():
     for cap in caps:
         capabilities[cap] = True
 
+def len_zh(data):
+    temp = re.findall('[^a-zA-Z0-9.\s]+', data)
+    count = 0
+    for i in temp:
+        count += len(i)
+    return(count)
+
 
 def show_dialog(content, sub):
     n = pynotify.Notification(content, sub)
     n.show()
 
-
 if __name__ == '__main__':
+    page_start = 0
+    channel_id = 0
+
     mdecode.initVM(classpath=mdecode.CLASSPATH)
     initCaps()
+    get_channel_list(page_start)
 
-    music_list = get_music_list()
+    while True:
+        print 'Enter ID select channel.'
+        print 'Enter "n" turn to next page or "p" turn to previous page.'
+        cmd = raw_input('Command:')
+        if cmd.isdigit():
+            channel_id = int(cmd)
+            music_list = get_music_list(channel_id)
+            if music_list == []:
+                print '[info] Not found channel.\n'
+            else:
+                break
+        else:
+            if cmd == 'n':
+                page_start += 1
+                if not get_channel_list(page_start):
+                    print '[info] List is already last page.\n'
+            elif cmd == 'p':
+                if page_start != 0:
+                    page_start -= 1
+                    get_channel_list(page_start)
+                else:
+                    print '[info] List is already first page.\n'
+
+
+
+
+    #music_list = get_music_list()
     for item in music_list:
         show_dialog(item['albumtitle'], item['artist'])
         if item == music_list[-1]:
-            music_list.extend(get_music_list())
+            music_list.extend(get_music_list(channel_id))
+        print '\nMusic Info'
+        print '==============='
+        print 'Name:', item['title']
+        print 'Album:', item['albumtitle']
+        print 'Artist', item['artist']
+        print 'Company', item['company']
+        print '===============\n'
         mdecode.Play.main([item['url']])
-        
