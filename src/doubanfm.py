774293db4804a611e2960b3959260e819a4516ef
mckelvin <ibmmc@live.com>
2013-03-12 21:57:42 +0800

    quick hack, support SkypeMood share...

--- src/doubanfm.py
+++ src/doubanfm.py
@@ -1,15 +1,25 @@
 # coding=utf-8
 
 import urllib
+import requests
 import json
 import pynotify
 import sys
 import time
-import mdecode
 import re
 import os
+import pygst
+pygst.require('0.10')
+import gst
+from util import getch
+from share import set_skype_status
 
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+COOKIE_PATH = os.path.join(ROOT_PATH, '.cookie')
+cookie = None
+if os.path.exists(COOKIE_PATH):
+    with open(COOKIE_PATH) as fh:
+        cookie = fh.read().strip()
 
 channelurl = 'http://douban.fm/j/explore/hot_channels'
 
@@ -24,7 +34,8 @@ music_list_params = {
             'type': 'n',
             'channel': '32',
             'from': 'mainsite',
-            'r': '73072e1c06',
+            'r': '720db0723e',
+            'kbps':'192',
         }
 
 def get_channel_list(page_num):
@@ -50,7 +61,7 @@ def get_channel_list(page_num):
     print ''.ljust(55, '-')
     return True
 
-def get_music_list(channel_id):
+def get_music_list(channel_id, type='n'):
     music_list_params['channel'] = str(channel_id)
     music_list = _request_url(fmurl, music_list_params)
     data = json.loads(music_list)
@@ -79,8 +90,12 @@ def played_music_log(music_info):
 
 def _request_url(url, params):
     params_data = urllib.urlencode(params)
-    data = urllib.urlopen('?'.join([url, params_data]))
-    return data.read()
+    url = '?'.join([url, params_data])
+    headers = {}
+    if cookie:
+        headers['Cookie'] = cookie
+    res = requests.get(url, headers ={'Cookie': cookie})
+    return res.text
 
 
 if not pynotify.init ("icon-summary"):
@@ -123,23 +138,25 @@ def show_dialog(content, sub):
     n.show()
 
 if __name__ == '__main__':
+
     page_start = 0
     channel_id = 0
 
-    mdecode.initVM(classpath=mdecode.CLASSPATH)
+    player = gst.element_factory_make("playbin", "player")
     initCaps()
 
+    current_pos = 0
     for i in range(len(sys.argv)):
         if sys.argv[i] == 'c':
             channel_id = sys.argv[i+1]
-
+    
+    '''
     if channel_id == 0:
         get_channel_list(page_start)
-
         while True:
             print 'Enter ID select channel.'
             print 'Enter "n" turn to next page or "p" turn to previous page.'
-            cmd = raw_input('Command:')
+            cmd = '1'#raw_input('Command:') TODO bug here
             if cmd.isdigit():
                 channel_id = int(cmd)
                 music_list = get_music_list(channel_id)
@@ -158,22 +175,67 @@ if __name__ == '__main__':
                         get_channel_list(page_start)
                     else:
                         print '[info] List is already first page.\n'
+    '''
+    channel_id = 0
+    print 'Channel: %s' % channel_id
+    music_list = get_music_list(channel_id)
 
-
-
+    bus = player.get_bus()
+    bus.add_signal_watch()
 
     music_list = get_music_list(channel_id)
-    for item in music_list:
+        
+    def play_song(pos=0):
+        item = music_list[pos]
         show_dialog(item['albumtitle'], item['artist'])
         if item == music_list[-1]:
             music_list.extend(get_music_list(channel_id))
-        print '\nMusic Info'
+        print ''
+        print 'Music Info'
         print '==============='
-        print 'Name:', item.get('title')
-        print 'Album:', item.get('albumtitle')
-        print 'Artist', item.get('artist')
-        print 'Company', item.get('company')
-        print '===============\n'
+        print 'Name: %s' % item.get('title')
+        print 'Album: %s' % item.get('albumtitle')
+        print 'Artist: %s' % item.get('artist')
+        print 'Company: %s' % item.get('company')
+        print '==============='
         played_music_log(item)
-        mdecode.Play.main([item['url']])
+        player.set_property('uri',item['url'])
+        player.set_state(gst.STATE_PLAYING)
+        set_skype_status('♪ 正在豆瓣FM上收听: %s - %s' % (item.get('artist'), item.get('title')))
+        
+
+    def on_message(bus, message):
+        global current_pos
+        t = message.type
+        if t == gst.MESSAGE_EOS:
+            player.set_state(gst.STATE_NULL)
+            current_pos += 1
+            play_song(current_pos)
+        elif t == gst.MESSAGE_ERROR:
+            player.set_state(gst.STATE_NULL)
+            current_pos += 1
+            play_song(current_pos)
+    bus.connect("message", on_message)
+    
+    play_song(0)
+    
+    while True:
+        c = getch()
+        if c == 'n':
+            player.set_state(gst.STATE_NULL)
+            music_list = get_music_list(channel_id, type='s') #skip
+            current_pos = 0
+            play_song(current_pos)
+        elif c == 'p':
+            player.set_state(gst.STATE_NULL)
+        elif c == 'P':
+            player.set_state(gst.STATE_PLAYING)
+        elif c == 'Q':
+            player.set_state(gst.STATE_NULL)
+            break
+        elif c == 'r':
+            music_list.extend(get_music_list(channel_id,type='r'))
+            print '❤\'ed %s' % music_list[current_pos].get('title') 
+
+        #TODO red ...
 
